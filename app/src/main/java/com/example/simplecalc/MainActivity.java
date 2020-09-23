05c45d6bb7872776b1f7827fc21b5a33a2c23a10
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2020-09-23 08:52:02 -0300

    Layout simple calc finished

--- app/src/main/java/com/example/simplecalc/MainActivity.java
+++ app/src/main/java/com/example/simplecalc/MainActivity.java
@@ -5,10 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+	private String value1;
+	private String value2;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		value1 = value2 = "";
+
 		setContentView(R.layout.activity_main);
 	}
 }
